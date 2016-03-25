4522e33baafc9f8213d996695a8f03cf88fdf3b9
patels13 <shreyap@sdpsoftware.com>
2016-03-24 23:17:09 -0400

    Create tests_markdown.py

---
+++ tests_markdown.py
@@ -0,0 +1,68 @@
+'''
+Test markdown.py with unittest
+To run tests:
+    python test_markdown_unittest.py
+'''
+
+import unittest
+from markdown_adapter import run_markdown
+
+class TestMarkdownPy(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_non_marked_lines(self):
+        '''
+        Non-marked lines should only get 'p' tags around all input
+        '''
+        self.assertEqual( 
+                run_markdown('this line has no special handling'), 
+                '<p>this line has no special handling</p>')
+
+    def test_em(self):
+        '''
+        Lines surrounded by asterisks should be wrapped in 'em' tags
+        '''
+        self.assertEqual( 
+                run_markdown('*this should be wrapped in em tags*'),
+                '<p><em>this should be wrapped in em tags</em></p>')
+
+    def test_strong(self):
+        '''
+        Lines surrounded by double asterisks should be wrapped in 'strong' tags
+        '''
+        self.assertEqual( 
+                run_markdown('**this should be wrapped in strong tags**'),
+                '<p><strong>this should be wrapped in strong tags</strong></p>')
+    def test_h1(self):
+        '''
+        Lines starting with #
+        '''
+        self.assertEqual(
+            run_markdown('# this should be wrapped in h tags'),
+            '<p><h1> this should be wrapped in h tags</h1></p>')
+    def test_h2(self):
+           '''
+           Lines starting with ##
+           '''  
+           self.assertEqual(
+               run_markdown('## this should be wrapped in h2 tags'),
+               '<p><h2> this should be wrapped in h2 tags</h2></p>') 
+    def test_h3(self):
+           '''
+           Lines starting with ###
+           '''
+           self.assertEqual(
+               run_markdown('### this should be wrapped in h3 tags\r\n### this should be wrapped in h3 tags'),
+               '<p><h3> this should be wrapped in h3 tags</h3></p> <p><h3> this should be wrapped in h3 tags</h3></p>')
+    def test_blockquote(self):
+            '''
+            Lines starting with >
+            '''
+            self.assertEqual(
+            run_markdown('> this should be wrapped in blockquote tags\r\n>hi'),
+            '<blockquote>\r\n<p> this should be wrapped in blockquote tags</p> <p>hi</p> </blockquote>')
+
+if __name__ == '__main__':
+    unittest.main()
